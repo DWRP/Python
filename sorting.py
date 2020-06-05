14eb8c1e21757a9d2ecef52ab5291821667b806f
Douglas Walas <douglas.skd@gmail.com>
2020-06-05 15:55:34 -0300

    finish struct mode

--- sorting.py
+++ sorting.py
@@ -1,17 +1,46 @@
-# O código começa sorteando um número aleatório.
-#O usuário começa digitando um número.
-#Se o número que ele digitou foi o número que o código sorteou, ele ganha.
-#O objetivo é acertar o número na menor quantidade de tentativas. Então tem que registrar também quantas vezes a pessoa tentou.
-#E esse código pode ou não dizer dicas, fica a seu critério...
+import random,os
 
-numero_comparacao = 0
-numero_usuario = input('Digite um número:\n')
+menu = True
+record = 0
 
-while (numero_usuario != str(numero_comparacao)):
-    
-    if (numero_usuario == str(numero_comparacao)):
-        print('Acertou')
-    else:
-        print('Errouuu!!')
-    
+while menu:
+    os.system('cls')
+    print("Recorde:", record)
+    numero_comparacao = random.randint(0,50)
+
+    contador = 0
+
     numero_usuario = input('Digite um número:\n')
+
+    while contador<15:
+        print(f'Número: {numero_comparacao}')
+        if (numero_usuario == str(numero_comparacao)):
+            print('Acertou')
+            print(f'Você tentou {contador} vezes!')
+            
+            if (contador < record):
+                record = contador
+            if(record == 0):
+                record = contador
+
+            contador = 15
+
+        else:
+            print('Errouuu!!')
+
+            if(numero_usuario<str(numero_comparacao)):
+                print('O número está acima')
+            else:
+                print('O número está abaixo')
+
+        if(contador<10):
+            numero_usuario = input('Digite um número:\n')
+        
+        contador += 1
+
+    print(f'Fim de jogo! Número: {numero_comparacao}')
+    
+    option = input('Deseja continuar a jogar?')
+
+    if (option.lower() != 's' and option.lower() != 'sim'):
+        menu = False
